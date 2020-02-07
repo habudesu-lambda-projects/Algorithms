f0ad08b0418e74aaf8fddac3039317f15775d893
Ken Ruf <ruf.kenichi@gmail.com>
2020-02-06 21:18:06 -0600

    completed rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return [[]]
+  elif n == 1:
+    return [['rock'], ['paper'], ['scissors']]
+  else:
+    rock = []
+    paper = []
+    scissors = []
+    for play in rock_paper_scissors(n-1):
+      rock += [['rock'] + play]
+      paper += [['paper'] + play]
+      scissors += [['scissors'] + play]
+    return rock + paper + scissors
+      
+
+print(rock_paper_scissors(2))
 
 
 if __name__ == "__main__":
